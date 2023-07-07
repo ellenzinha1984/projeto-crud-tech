b6b46e943fcb0863da2d6e2ec1fa7c5a1f10b26a
Renato Pejon <pokeracre@gmail.com>
2023-07-07 16:07:21 -0300

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,34 @@
+function loadTable() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
+    xhttp.open("GET", "http://127.0.0.1:8000/pets/todos");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        
+        var trHTML = "";
+        const objects = JSON.parse(this.responseText);
+        for (let object of objects) {
+          trHTML += "<tr>";
+          trHTML += "<td>" + object["nome"] + "</td>";
+          trHTML += "<td>" + object["especie"] + "</td>";
+          trHTML += "<td>" + object["raca"] + "</td>";
+          trHTML += "<td>" + object["cor"] + "</td>";
+          trHTML += "<td>" + object["idade"] + "</td>";
+          trHTML += "<td>" + object["tutor"] + "</td>";
+          trHTML +=
+            '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
+            object["id"] +
+            ')">Edit</button>';
+          trHTML +=
+            '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' +
+            object["id"] +
+            ')">Del</button></td>';
+          trHTML += "</tr>";
+        }
+        document.getElementById("mytable").innerHTML = trHTML;
+      }
+    };
+}
+  
+loadTable();
---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+import pymongo, json
+from bson import json_util
+from bson import ObjectId
+from pydantic import BaseModel
+
+client = pymongo.MongoClient("mongodb+srv://renatobessa:carcrud123@carcrud.smxhds1.mongodb.net/")
+db = client["Doguinhos"]
+
+class Dog(BaseModel):
+    nome: str
+    raca: str
+    especie: str
+    cor: str
+    idade: int
+    tutor: str
+
+app = FastAPI()
+
+@app.post("/pets/")
+async def cadastra_pets(dog: Dog):
+    dog_dict = dog.dict()
+    db.doguinhos.insert_one(dog_dict)
+    return {"message": "Pet cadastrado com sucesso"}
+
+@app.get("/pets/todos")
+async def lista_pets():
+    dog = db.doguinhos.find({})
+
+    if dog:
+        dog_json = json_util.dumps(dog)
+        dog_dict = json.loads(dog_json)
+        return dog_dict
+    else:
+        return {"message": "Pet não encontrado"}
+    
+@app.get("/pets/{id_pet}")
+async def lista_pet_especifico(id_pet: str):
+    dog = db.doguinhos.find_one({"_id": ObjectId(id_pet)})
+
+    if dog:
+        dog_json = json_util.dumps(dog)
+        dog_dict = json.loads(dog_json)
+        return dog_dict
+    else:
+        return {"message": "Pet não encontrado"}
+    
+
+@app.put("/pets/atualiza/{id_pet}")
+async def edita_pet(id_pet: str, dog: Dog):
+    db.doguinhos.update_one({"_id": ObjectId(id_pet)}, {"$set": dog.dict()})
+
+    return {"message": "Pet ATUALIZADO"}
+
+
+@app.delete("/pets/deleta/{id_pet}")
+async def exclui_pet(id_pet: str):
+    resultado = db.doguinhos.delete_one({"_id": ObjectId(id_pet)})
+
+    if resultado.deleted_count == 1:
+        return {"message": "Pet foi deletado com sucesso!"}
+    else:
+        return{"message": "Pet não deletado!"}
